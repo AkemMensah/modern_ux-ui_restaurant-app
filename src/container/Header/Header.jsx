55931b806efa658e408e4e3ede28990e53916eac
AkemMensah <141195emmanuel@gmail.com>
2024-06-22 19:49:55 +0000

    created the header component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Feel the new taste" />
+      <h1 className="app__header-h1">It's not just Dining, It's an Experience</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0',color:"",fontSize:"16px",fontStyle:"" }}>Savor the Taste, Indulge in the Experience: Welcome to Culinary Delights! A taste of Refreshment</p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
   </div>
 );
 
