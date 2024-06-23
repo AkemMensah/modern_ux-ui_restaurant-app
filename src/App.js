8d5f05e4edadc2ed3ca021790fd1e3cb8daa86e5
AkemMensah <141195emmanuel@gmail.com>
2024-06-23 16:29:26 +0000

    Added routing to enable navigation to other pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
+// import { Routes, Route } from "react-router-dom";
+import BookingPage from './components/BookingPage/BookingPag';
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
-);
+
+function HomePage() {
+  return (
+      <>
+        <Navbar />
+        <Header />
+        <AboutUs />
+        <SpecialMenu />
+        <Chef />
+        <Intro />
+        <Laurels />
+        <Gallery />
+        <FindUs />
+        <Footer />
+      </>
+  );
+}
+
+function BookingElement(){
+  return (
+    <>
+    < BookingPage />
+    < Footer />
+    </>
+  )
+}
+
+const App = () => {
+  return (
+  <Router>
+    <>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/booking-form" element={<BookingElement />} />
+      </Routes>
+    </>
+  </Router>
+)}; 
 
 export default App;
