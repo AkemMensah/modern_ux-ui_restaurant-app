0ee90cfa3facf2c436c7c5975a237fd301abc339
AkemMensah <141195emmanuel@gmail.com>
2024-06-23 16:26:48 +0000

    added the styles to the footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,13 +13,14 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">Kumasi,Ashanti Region,Ghana. Box B219</p>
+        <p className="p__opensans">+233 248943700</p>
+        <p className="p__opensans">+233 547495692</p>
       </div>
 
       <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="footer_logo" />
+        {/* <img src={images.gericht} alt="footer_logo" /> */}
+        <h1 style={{color:"#ffffff", marginBottom:"2rem"}}>LitleBee</h1>
         <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
         <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
         <div className="app__footer-links_icons">
@@ -39,7 +40,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2025 LitleBee. All Rights reserved.</p>
     </div>
 
   </div>
